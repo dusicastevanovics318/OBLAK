600c2eda6393b684d3da04e84c70673e0ec41429
Dusica Stevanovic <56299347+dusicastevanovics318@users.noreply.github.com>
2021-05-29 23:49:58 +0200

    Create dusica.sh
    napravljena skripta

---
+++ dusica.sh
@@ -0,0 +1,21 @@
+curl --location --request POST 'https://getpantry.cloud/apiv1/pantry/bfab2361-7ccc-44d6-8212-62e1b30b244a/basket/dusicaraf' 
+--header 'Content-Type: application/json' 
+--data-raw 
+
+{
+"id": "1abcd1",
+"ime": "dusica",
+"prezime": "stevanovic",
+"smer": "IT",
+"predmet": [
+"SQL",
+"C#",
+"PWA"
+],
+"prosek": 8.5,
+"kontakt": {
+"adresa": "ulica broj 1",
+"mesto": "beograd",
+"telefon": "060123456"
+}
+}
